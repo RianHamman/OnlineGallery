509067c2cde12e7f2692068314599b84b75171c2
DCGHTS <89462338+DCGHTS@users.noreply.github.com>
2021-11-21 18:32:41 +0200

    download done

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly string wwwrootDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+        public IActionResult Index()
+        {
+            List<string> images = Directory.GetFiles(wwwrootDirectory, "*.png").Select(Path.GetFileName).ToList();
+            return View(images);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile myFile)
+        {
+            if (myFile != null)
+            {
+                var path = Path.Combine(wwwrootDirectory, DateTime.Now.Ticks.ToString() + Path.GetExtension(myFile.FileName));
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await myFile.CopyToAsync(stream);
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> DownloadFile(string filePath)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", filePath);
+            var memory = new MemoryStream();
+
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            var contentType = "APPLICATION/octet-stream";
+            var fileName = Path.GetFileName(path);
+
+            return File(memory, contentType, fileName);
+        }
+    }
+}
