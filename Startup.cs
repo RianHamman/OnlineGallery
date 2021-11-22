dff8d8eee90648b1f9f7c4c0b43981ef59e8bead
DCGHTS <89462338+DCGHTS@users.noreply.github.com>
2021-11-22 08:44:17 +0200

    update

--- Startup.cs
+++ Startup.cs
@@ -43,15 +43,15 @@ namespace OnlineGallery
             services.AddMvc();
             var connection = @"Server=(localdb)\\mssqllocaldb;Database=OnlineGalleryProject;Trusted_Connection=True;MultipleResultSets=True";
             services.AddDbContext<MyContext>(options => options.UseSqlServer(connection));
-            //services.AddDbContext<MyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            /*services.AddDbContext<ctx>(Options => Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); //used to link db in startup. 
+            /*services.AddDbContext<MyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ctx>(Options => Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); //used to link db in startup. 
             services.AddTransient<IUnitWork, UnitWork>();
             var config = new AutoMapper.MapperConfiguration(cfg => 
             {
                 cfg.AddProfile(new MyProfile());
             });
             var mapper = config.CreateMapper();
-            services.AddSingleton(mapper); 
+            services.AddSingleton(mapper); */
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
