c29d912e48982c6963727152f76e3db5160deffa
DCGHTS <89462338+DCGHTS@users.noreply.github.com>
2021-11-22 18:33:40 +0200

    tages done

--- ViewModels/ImageViewModels/ImageCreateViewModel.cs
+++ ViewModels/ImageViewModels/ImageCreateViewModel.cs
@@ -4,5 +4,9 @@
     {
         public List<IFormFile> Files { get; set; }
         public int AlbumId { get; set; }
+        public int Tags { get; set; }
+        public int Geolocation { get; set; }
+        public int CreatedBy { get; set; }
+        public int CreatedDate { get; set; }
     }
 }
